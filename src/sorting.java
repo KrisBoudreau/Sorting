0cd63dee40d8f6cf637cd4dfaddb4929300a04f6
KrisBoudreau <83515238+KrisBoudreau@users.noreply.github.com>
2022-12-29 00:41:05 -0500

    bubble sort

--- src/sorting.java
+++ src/sorting.java
@@ -1,7 +1,49 @@
-public class sorting {
+import java.util.Random;
 
+public class sorting {
     public static void main(String[] args) {
-        System.out.println("helo");
+        
+        Random r = new Random();
+        int size = r.nextInt(20);
+        int [] a = new int[size];
+
+        for (int i = 0; i < a.length; i++)
+            a[i] = r.nextInt(100);
+        
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+
+        System.out.println(" ");
+
+        bubble(a);
+        
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+
+
+
+    
+    }
+
+    public static void bubble(int[] a){
+        boolean sorted = true;
+        for (int i = 0; i < a.length - 1; i++){
+            for (int j = 0; j < a.length - 1 - i; j++){
+                if (a[j] > a[j+1]){
+                    swap(a, j, j+1);
+                    sorted = false;
+                }
+            }
+            if (sorted){
+                break;
+            }
+        }
+    }
+
+    public static void swap(int[] a, int i, int j){
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
     
 }
